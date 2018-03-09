baff998806fbdfcef699b8b8a9926da6c0080fa5
VranceanuM <markdevstudio@gmail.com>
2018-03-09 16:35:30 +0300

    Add files via upload

--- particle.js/js/main.js
+++ particle.js/js/main.js
@@ -1,5 +1,27 @@
-$(document).ready(function () {
-    // Custom JS & jQuery here
-    $('.button-collapse').sideNav();
-        
-});
+//1st text
+var element = $(".element");
+
+        $(function() {
+            element.typed({
+                strings: ["OUR WELL-DEFINE", "DEVELOPMENT PROCESSES."],
+                typeSpeed: 100,
+                loop: true,
+            });
+        });
+    //resize
+
+
+    //scroll
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+         //>=, not <=
+        if (scroll >= 550) {
+            //clearHeader, not clearheader - caps H
+            $('nav').addClass("navbar-inverse");
+        }
+    });
+    $( window ).resize(function() {
+        if($(window).width() <=762) $('nav').addClass("navbar-inverse");
+        else $('nav').removeClass("navbar-inverse");
+      });
